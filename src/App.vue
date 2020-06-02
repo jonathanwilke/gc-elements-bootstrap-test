f5c5f4e69a9e88d2cdf0e8346e9bfb5ca22edfd4
Jonathan Wilke <info@jonathan-wilke.de>
2020-06-02 16:49:10 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="gc-element">
+    <b-card class="rounded-lg" style="background-color: #111;">
+      <div class="pa-6">
+        <b-row align-v="center">
+          <b-col cols="12" md="6" lg="5" class="text-center">
+            <div class="mx-auto w-auto">
+              <b-img
+                style="max-width:250px"
+                src="https://images.prismic.io/jonathan-wilke/6dde37c3-42c7-4a5d-9aa9-49b6862ee9c4_work-finally-friday-logo.png?auto=compress,format&amp;w=300&amp;auto=compress"
+                alt="Finally Friday"
+                loading="lazy"
+                fluid
+              />
+            </div>
+            <div
+              class="mt-4 text-primary font-weight-bold text-uppercase tag-list font-sm"
+            >
+              <span class="d-inline-block">#Design</span>
+              <span class="d-inline-block pl-2">#Wordpress</span>
+            </div>
+            <b-btn pill variant="primary" class="mt-4" v-b-modal.modal-1
+              >Seite ansehen</b-btn
+            >
+          </b-col>
+          <b-col cols="12" md="6" lg="7">
+            <b-img
+              class="max-w-full"
+              src="https://images.prismic.io/jonathan-wilke/6812cb85-387d-4140-ab17-c09518d6eb27_work-finally-friday.png?auto=compress,format&amp;w=800&amp;auto=compress"
+              alt="Finally Friday"
+              loading="lazy"
+              fluid
+            />
+          </b-col>
+        </b-row>
+      </div>
+    </b-card>
+    <b-modal id="modal-1" title="Toller Dialog" ok-only>
+      Das hier ist ein wunderbarer Dialog zum Testen des Elements und der
+      Komponenten-Bibliothek!
+    </b-modal>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.tag-list {
+  font-size: 0.875rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { BootstrapVue } from "bootstrap-vue";
+import "./assets/styles.scss";
 
+// Install BootstrapVue
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#gc-element-wrapper-bootstrap");
